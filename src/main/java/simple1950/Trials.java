c671df1f54b632727704c845bc55e97f0b090809
DELL <DELL@DESKTOP-T6812S3>
2022-04-13 18:41:40 +0530

    Trial push

---
+++ src/main/java/simple1950/Trials.java
@@ -0,0 +1,15 @@
+package simple1950;
+
+public class Trials {
+	
+	public static void trialMethod() {
+		int a =10;
+		int b = 20;
+		int c = a+b;
+		System.out.println(c);
+
+	}
+public static void main(String[] args) {
+	trialMethod();
+}
+}
